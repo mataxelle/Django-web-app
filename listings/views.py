6cb051196008a5b5eccf1feac33467e30cb095c3
axelle <kadance9720@gmail.com>
2025-04-23 09:15:07 +0200

    Create first page

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django !</h1>')
+
+def about(request):
+    return HttpResponse('<h1>A propos</h1> <p>Ceci est une page à propos !</p>')
+
+def contact(request):
+    return HttpResponse('<h1>Contact</h1> <p>Ceci est une page de contact !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Listings</h1> <p>Ceci est une page de liste d\'annonces !</p>')
