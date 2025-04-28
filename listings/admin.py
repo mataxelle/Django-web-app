6e6b840d1732f9bfe1c7f13dc329c26b23a44cd8
axelle <kadance9720@gmail.com>
2025-04-28 15:23:22 +0200

    Update Brand model and create listing model

--- listings/admin.py
+++ listings/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from listings.models import Brand
+from listings.models import Brand, Listing
 
 class BandAdmin(admin.ModelAdmin):
     list_display = ('name', 'year_created')
 
 admin.site.register(Brand, BandAdmin)
+admin.site.register(Listing)
--- listings/models.py
+++ listings/models.py
@@ -14,7 +14,7 @@ class Brand(models.Model):
         USA = 'Étas-Unis'
         Mexique = 'Mexique'
         Autre = 'Autre'
-    
+
     class MerchType(models.TextChoices):
         Vêtement = 'Vêtement'
         Chaussure = 'Chaussure'
@@ -28,7 +28,22 @@ class Brand(models.Model):
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
     creator = models.fields.CharField(max_length=100, null=True, blank=True)
+
+class Listing(models.Model):
+
+    def __str__(self):
+        return f'{self.name}'
+    
+    class MerchType(models.TextChoices):
+        Vêtement = 'Vêtement'
+        Chaussure = 'Chaussure'
+        Poster = 'Poster'
+        Divers = 'Divers'
+
+    name = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
     sold = models.fields.BooleanField(default=False)
     year = models.fields.IntegerField(validators=[MinValueValidator(1850), MaxValueValidator(2024)])
     type = models.fields.CharField(max_length=100, choices=MerchType.choices)
+    brand = models.ForeignKey(Brand, null=True, on_delete=models.SET_NULL)
 
